606db93f776cce9e4e1a58d3c789fc9564544508
cebarizon <cebarizon@575e5654-c610-811f-3838-ac9926e9de97>
2011-07-09 13:11:03 +0000

    Testing scale transition in ellipses.

---
+++ trunk/ReactFace/src/br/com/manish/reactFace/example/MyEllipse.java
@@ -0,0 +1,50 @@
+package br.com.manish.reactFace.example;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Ellipse;
+import br.com.manish.reactFace.core.ReactFaceObject;
+
+
+public class MyEllipse extends Ellipse implements ReactFaceObject{
+	
+	public MyEllipse (Color color) {
+        super(90, 45);
+        LinearGradient gradient = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[] {
+            new Stop(0, color),
+            new Stop(1, Color.BLACK)
+        });
+        setStroke(color);
+        setFill(gradient);
+        setVisible(false);
+	}
+	public MyEllipse() {
+		this(Color.DODGERBLUE);
+	}
+
+	@Override
+	public void move(Double x, Double y) {
+		setTranslateX(x);
+		setTranslateY(y);		
+	}
+
+	@Override
+	public void show() {
+		setVisible(true);
+	}
+
+	@Override
+	public void hide() {
+		setVisible(false);
+	}
+	
+	@Override
+	public void rotate(Double angle) {
+		setRotate(angle);
+	}
+}
+
+
+
